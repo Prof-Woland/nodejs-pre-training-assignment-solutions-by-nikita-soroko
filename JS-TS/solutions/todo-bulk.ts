6701c9461c96a9d0e11b3212e47d3b3910c0482b
Prof-Woland <sorokonikita08@gmail.com>
2026-06-13 10:48:18 +0300

    Solution #5 in JS-TS

--- JS-TS/solutions/todo-bulk.ts
+++ JS-TS/solutions/todo-bulk.ts
@@ -1,13 +1,33 @@
 import { Todo, TodoStatus } from './types';
 
 export function toggleAll(state: Todo[], completed: boolean): Todo[] {
-  throw new Error('toggleAll: not implemented');
+  if(typeof state === null || typeof state === undefined){
+    throw new TypeError('Todo array is null or undefined');
+  };
+
+  const status: TodoStatus = completed ? TodoStatus.COMPLETED : TodoStatus.IN_PROGRESS;
+
+  const result: Todo[] = state.map(e => {return {...e, status}});
+
+  return result;
 }
 
 export function clearCompleted(state: Todo[]): Todo[] {
-  throw new Error('clearCompleted: not implemented');
+  if(typeof state === null || typeof state === undefined){
+    throw new TypeError('Todo array is null or undefined');
+  };
+
+  const result: Todo[] = state.filter(e => e.status !== TodoStatus.COMPLETED);
+
+  return result;
 }
 
 export function countByStatus(state: Todo[], status: TodoStatus): number {
-  throw new Error('countByStatus: not implemented');
+  if(typeof state === null || typeof state === undefined){
+    throw new TypeError('Todo array is null or undefined');
+  };
+
+  const result: number = state.reduce((acc, curr) => {return curr.status === status ? acc + 1 : acc}, 0);
+
+  return result;
 }
