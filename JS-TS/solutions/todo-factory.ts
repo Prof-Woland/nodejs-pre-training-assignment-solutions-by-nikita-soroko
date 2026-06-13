f3f10519a75b72aaebc59193acb9a71395a4f1e9
Prof-Woland <sorokonikita08@gmail.com>
2026-06-13 10:04:19 +0300

    Solution #3 in JS-TS

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -3,5 +3,18 @@ import { Todo, NewTodo, TodoStatus } from './types';
 let nextId = 1;
 
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+  if(typeof input === null || typeof input === undefined){
+    throw new TypeError('Input is null or undefined');
+  }
+
+  const result: Todo = {
+    id: nextId,
+    status: TodoStatus.PENDING,
+    ...input,
+    createdAt: new Date(Date.now()),
+  }
+
+  nextId++;
+
+  return result;
 }
